196e7fcc6ab661066f7b169e7c259cc7246d985c
santiagods1980 <santiagodurazno@gmail.com>
2026-07-09 15:08:44 -0500

    Fix image filename case in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Aplicación de Fundamentos Streamlit")
 st.sidebar.title("Parámetros")
-st.image("REDYCOMPU - IMAGEN PORTADA FACEBOOK.PNG")
+st.image("REDYCOMPU - imagen portada facebook.png")
